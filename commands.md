a3dff072843e449b457efaa624c6ef8f696ba8bb
Fan Hongtao <fanhongtao@gmail.com>
2021-02-26 12:34:22 +0800

    update parse.sh, for new commands.

--- commands.md
+++ commands.md
@@ -1,2 +1,7 @@
+`find {{root_path}} -name '{{*.ext}}' -exec {{wc -l {} }}\;`
 `find {{root_path}} -name '{{*.ext}}'`
 `wget --limit-rate={{300k}} --tries={{100}} {{https://example.com/somepath/}}\`
+
+`{{arguments_source}} | xargs sh -c "{{command1}} && {{command2}} | {{command3}}"`
+`find . -name {{'*.backup'}} -print0 | xargs -0 rm -v`
+
--- parse.sh
+++ parse.sh
@@ -1,43 +1,92 @@
 #!/bin/sh
 # Test code for parse command page's command.
-
+#set -x
 unquote() {
 	# take everything between two backticks, capture it
 	printf "%s\n" "$*" | sed "s/\`\([^\`]*\)\`/\1/g"
 }
 
+# contains string substr
+# return: true, if `string` contains `substr`.
+contains() {
+	[ -z $2 ] && { echo "true"; return; }
+	[ "${1#*$2*}" = "$1" ] && echo "false" || echo "true"
+}
+
+# startswith string prefix
+# return: true, if `string` start with `prefix`
+startswith() {
+	[ "$2${1#${2}}" = "$1" ] && echo "true" || echo "false"
+}
+
+log() {
+	printf ""
+	# echo $*
+}
+
 print_value() {
+	# Delete first character of "$*", and check if the rest string contain $value_end_tag
+	# Beacuse value start with `'` or `"`, end tag is the same as start tag. So we should check after delete the first character.
+	if $(contains `echo "$*" | sed 's/.//'` $value_end_tag); then
+		want_value=false
+	else
+		want_value=true
+	fi
 	printf "${svalue}"
 	printf "%s" "$*" | sed "s/{{\(.*\)}}/\1/g"
 	printf "${reset}"
+	log "value_end_tag: $value_end_tag, want_value: $want_value"
 }
 
 parse() {
-	printf "  "
+	value_end_tag=""
+	want_value=false
+	printf " "
 	for word in $*; do
+		printf " "
+		log "want_value: $want_value"
+		if $want_value; then
+			print_value "$word"
+			continue
+		fi
+
 		case "$word" in
 		\{\{*) # vlaue
+			value_end_tag="}}"
 			print_value "$word"
 			;;
 		\'*) # vlaue
+			value_end_tag="'"
+			print_value "$word"
+			;;
+		\"*) # vlaue
+			value_end_tag='"'
 			print_value "$word"
 			;;
 		-*) # option
 			new=`echo "$word" | sed "s/=//"`
 			if [ $new = $word  ]; then
-				printf "${sparam}$word${reset}"
+				printf "${sparam}${word}${reset}"
 			else
 				option=${word%%=*}
 				value=${word##*=}
 				printf "${sparam}${option}=${reset}"
+				if $(startswith $value "{{"); then
+					value_end_tag="}}"
+				elif $(startswith $value "'"); then
+					value_end_tag="'"
+				elif $(startswith $value '"'); then
+					value_end_tag='"'
+				else
+					value_end_tag=""
+				fi
 				print_value "$value"
 			fi
 			;;
 		*)
-			printf "${scode}$word${reset}"
+			printf "${scode}${word}${reset}"
 			;;
 		esac
-		printf " "
 	done
 	echo
 }
@@ -54,7 +103,11 @@ scode="\33[31m" # red
 sparam="\33[33m"
 svalue="\33[36m"
 
-cat commands.md | while read line; do
+# cat commands.md | while read line; do
+cat commands.md | while IFS= read -r line || [ -n "$line" ]; do
+	if [ -z "$line" ]; then
+		continue
+	fi
 	echo $line
 	code "$line"
 	echo
